286ccfd8d7f1a1f1688a2e0fb6d270ed65fe5516
nivedha75 <35554854+nivedha75@users.noreply.github.com>
2018-04-13 17:29:44 -0700

    started code

--- autostack.c
+++ autostack.c
@@ -16,6 +16,8 @@ task main()
 //make a function that 1. lifts the robot by setting the motor to the highest speed for 2 seconds * the count number
 //2. increment by 1 for count variable
 //3. multiply count variable by 2 seconds and set motors to go in reverse
+	
+if (
 
 
 
