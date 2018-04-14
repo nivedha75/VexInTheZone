6bff620d73bcef6788109e32e847e65a419c361f
nivedha75 <35554854+nivedha75@users.noreply.github.com>
2018-04-13 17:25:13 -0700

    some pseudo code
    commented

---
+++ autostack.c
@@ -0,0 +1,22 @@
+
+task main()
+{
+//autostack file
+//make a variable that determines the height of the robot
+	//bc we won't be able to determine exactly how tall the robot is in starting position at the loader
+	//we would need to make the height of the robot variable the height added from original position
+	//thus this variable would be set to zero first
+
+//then have a variable that counts the number of times you lift the robot
+
+//how to write the functions:
+
+
+//make while function that says has an if pressed function
+//make a function that 1. lifts the robot by setting the motor to the highest speed for 2 seconds * the count number
+//2. increment by 1 for count variable
+//3. multiply count variable by 2 seconds and set motors to go in reverse
+
+
+
+}
